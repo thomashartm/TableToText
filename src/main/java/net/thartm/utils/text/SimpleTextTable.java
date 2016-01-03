d7fd6894bc7106a2b91ab2a93cbf0bd0ddda43eb
Thomas Hartmann <thomas.hartmann@netcentric.biz>
2016-01-04 00:02:59 +0100

    Initual commit

---
+++ src/main/java/net/thartm/utils/text/SimpleTextTable.java
@@ -0,0 +1,115 @@
+package net.thartm.utils.text;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author thomas.hartmann@netcentric.biz
+ * @since 01/2016
+ */
+public class SimpleTextTable {
+
+    private static final String WHITESPACE = " ";
+
+    private static final String DASH = "-";
+
+    private int maxColumns = -1;
+
+    private int[] maxColumnWidths;
+
+    private List<String[]> rows = new LinkedList<String[]>();
+
+    private boolean showHeader;
+
+    private boolean showLineNumbers;
+
+    public SimpleTextTable() {
+    }
+
+    public SimpleTextTable(final boolean showHeader, final boolean showLineNumbers) {
+        this.showHeader = showHeader;
+        this.showLineNumbers = showLineNumbers;
+    }
+
+    public void addRow(final String... cols) {
+        calculateColumnWidths(cols);
+
+        rows.add(cols);
+    }
+
+    private void calculateColumnWidths(final String[] cols) {
+        if (cols.length > this.maxColumns) {
+            this.maxColumns = cols.length;
+            maxColumnWidths = new int[cols.length];
+        }
+
+        for (int i = 0; i < cols.length; i++) {
+            this.maxColumnWidths[i] = Math.max(this.maxColumnWidths[i], StringUtils.length(cols[i]));
+        }
+    }
+
+    public String printFormatted() {
+        final StringBuilder sb = new StringBuilder();
+
+        final int sizeForLineNumbers = showLineNumbers ? String.valueOf(rows.size()).length() : -1;
+
+        for (int rowNum = 0; rowNum < rows.size(); rowNum++) {
+            final String[] row = rows.get(rowNum);
+
+            showHeader(sb, rowNum, row);
+
+            showLineNumbers(sb, sizeForLineNumbers, rowNum);
+
+            for (int colNum = 0; colNum < row.length; colNum++) {
+                final String value = StringUtils.defaultString(row[colNum]);
+
+                final int columnWidth = maxColumnWidths[colNum];
+
+                sb.append(StringUtils.rightPad(value, columnWidth, WHITESPACE));
+                sb.append(WHITESPACE);
+
+            }
+
+            sb.append(StringUtils.LF);
+        }
+
+        return sb.toString();
+    }
+
+    private void showLineNumbers(final StringBuilder sb, final int sizeForLineNumbers, final int rowNum) {
+        if (showLineNumbers) {
+
+            if (rowNum > 0) {
+                sb.append(StringUtils.rightPad(String.valueOf(rowNum), sizeForLineNumbers, WHITESPACE));
+                sb.append(WHITESPACE);
+            }else {
+                sb.append(StringUtils.rightPad(WHITESPACE, sizeForLineNumbers, WHITESPACE));
+            }
+        }
+    }
+
+    private void showHeader(final StringBuilder sb, final int rowNum, final String[] row) {
+        if (showHeader && rowNum == 1) {
+            for (int colNum = 0; colNum < row.length; colNum++) {
+                final int columnWidth = maxColumnWidths[colNum];
+                sb.append(StringUtils.rightPad(DASH, columnWidth, DASH));
+                sb.append(WHITESPACE);
+            }
+            sb.append(StringUtils.LF);
+        }
+    }
+
+    public int getMaxColumns() {
+        return maxColumns;
+    }
+
+    public int[] getMaxColumnWidths() {
+        return maxColumnWidths;
+    }
+
+    public List<String[]> getRows() {
+        return rows;
+    }
+}
---
+++ src/test/java/net/thartm/utils/text/SimpleTextTableTest.java
@@ -0,0 +1,25 @@
+package net.thartm.utils.text;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author thomas.hartmann@netcentric.biz
+ * @since 01/2016
+ */
+public class SimpleTextTableTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testAddRow() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testPrintFormatted() throws Exception {
+
+    }
+}
