9d7759ade5f0c425ef87adc02f8805c8940b8eaa
Thomas Hartmann <thomas.hartmann@netcentric.biz>
2016-01-04 10:51:42 +0100

    Updated unit test

--- src/test/java/net/thartm/utils/text/SimpleTextTableTest.java
+++ src/test/java/net/thartm/utils/text/SimpleTextTableTest.java
@@ -1,5 +1,7 @@
 package net.thartm.utils.text;
 
+import org.junit.Assert;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,24 +11,31 @@ import org.junit.Test;
  */
 public class SimpleTextTableTest {
 
+    private SimpleTextTable table;
+
     @Before
     public void setUp() throws Exception {
-
+        this.table = new SimpleTextTable(true, true);
+        table.addRow("Column Header 1", "Column Header 2", "Column Header 1");
+        table.addRow("Row 1 Value 1", "Row 1 Value 2", "Row 1 Value 3");
+        table.addRow("Row 2 Value 1", "Row 2 Value 2", "Row 2 Value 3");
+        table.addRow("Row 2 Value 1", "Row 2 Value 2", "Row 2 Value 3");
     }
 
     @Test
     public void testAddRow() throws Exception {
-
+        Assert.assertEquals(4, table.getRows().size());
     }
 
     @Test
     public void testPrintFormatted() throws Exception {
-        SimpleTextTable simpleTextTable = new SimpleTextTable(true, true);
-        simpleTextTable.addRow("Column Header 1", "Column Header 2", "Column Header 1");
-        simpleTextTable.addRow("Row 1 Value 1", "Row 1 Value 2", "Row 1 Value 3");
-        simpleTextTable.addRow("Row 2 Value 1", "Row 2 Value 2", "Row 2 Value 3");
-        simpleTextTable.addRow("Row 2 Value 1", "Row 2 Value 2", "Row 2 Value 3");
 
-        System.out.println(simpleTextTable.printFormatted());
+        final String expected = " Column Header 1 Column Header 2 Column Header 1 " + StringUtils.LF
+                + "--------------- --------------- --------------- " + StringUtils.LF
+                + "1 Row 1 Value 1   Row 1 Value 2   Row 1 Value 3   " + StringUtils.LF
+                + "2 Row 2 Value 1   Row 2 Value 2   Row 2 Value 3   " + StringUtils.LF
+                + "3 Row 2 Value 1   Row 2 Value 2   Row 2 Value 3   " + StringUtils.LF;
+
+        Assert.assertEquals(expected, table.printFormatted());
     }
 }
